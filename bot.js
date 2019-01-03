b1e247705469bc86c57e72fa03f3dae85ee97b29
Ryder McMinn <mcminnra@ryders-mbp.attlocal.net>
2019-01-03 16:00:19 -0500

    revert

--- bot.js
+++ bot.js
@@ -414,7 +414,7 @@ function respond(req, res, db) {
         return;
     } 
     // /news
-    else if((request.text && botRegexNews.test(request.text)) || newsCheck(db)) {
+    else if((request.text && botRegexNews.test(request.text))) {
         console.log(newsCheck(db))
 
         console.log("Command => /news")
