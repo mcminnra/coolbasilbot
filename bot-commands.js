f397af03bdefe59499c94b14b7e18d14eb3e1a72
Ryder McMinn <mcminnra@gmail.com>
2019-07-03 23:01:34 -0700

    fix return because I suck at JS

--- bot-commands.js
+++ bot-commands.js
@@ -3,7 +3,7 @@ const giphy = require('giphy-api')(process.env.GIPHY_KEY);
 function chelKeyword() {
     giphy.random('nhl', function (err, resGif) {
         if (err) console.log(err);
-        return(resGif.data.image_url);
+        return resGif.data.image_url;
     });
 }
 
