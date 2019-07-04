2a17e718584cb13036edffbce47a8473acd53226
Ryder McMinn <mcminnra@gmail.com>
2019-07-03 23:13:53 -0700

    more log test 3

--- bot-commands.js
+++ bot-commands.js
@@ -3,7 +3,7 @@ const giphy = require('giphy-api')(process.env.GIPHY_KEY);
 /*
     Keywords
 */
-function chelKeyword() {
+async function chelKeyword() {
     giphy.random('nhl', function (err, resGif) {
         if (err) console.log(err);
         return resGif.data.image_url;
