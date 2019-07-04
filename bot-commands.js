86dc25a5b134acff788e9b593a41601b0a40fdfc
Ryder McMinn <mcminnra@gmail.com>
2019-07-03 22:58:54 -0700

    third time the charm

--- bot-commands.js
+++ bot-commands.js
@@ -1,10 +1,12 @@
-export function chelKeyword() {
+const giphy = require('giphy-api')(process.env.GIPHY_KEY);
+
+function chelKeyword() {
     giphy.random('nhl', function (err, resGif) {
         if (err) console.log(err);
         return(resGif.data.image_url);
     });
 }
 
-export default {
+module.exports = {
     chelKeyword
 }
